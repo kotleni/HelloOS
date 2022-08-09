22a6caecf0d5d99e0856412bd9285945a9a78495
kotleni <yavarenikya@gmail.com>
2022-08-09 12:47:03 +0300

    Updated motd

--- kernel/src/include/defines.h
+++ kernel/src/include/defines.h
@@ -1,5 +1,5 @@
 /* HelloOS */
 
-const char motd[] = "Welcome to HelloOS";
+const char motd[] = "HelloOS";
 const char version[] = "0.0.4[test]"; // version string
 const char shell[] = "\n% ";      // shell prompt message
