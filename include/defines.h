23bce097a7962203ebe0ca96e341051724106595
kotleni <yavarenikya@gmail.com>
2024-01-15 18:21:49 +0200

    refactor + changed version

--- include/defines.h
+++ include/defines.h
@@ -1,5 +1,4 @@
-/* HelloOS */
+/* defines.h - Kernel constants header file. */
 
-const char motd[] = "HelloOS";
-const char version[] = "0.0.4[test]"; // version string
-const char shell[] = "\n% ";      // shell prompt message
+const char version[] = "0.0.5[test]";   // version string
+const char shell[] = "\n% ";            // shell prompt message
