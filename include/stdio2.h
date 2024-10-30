bdd3c2d55756fca49a2e46a0c5bde034ab14716f
Viktor Varenik <yavarenikya@gmail.com>
2024-10-30 18:05:46 +0200

    chore(refactor): fixed all warnings

--- include/stdio2.h
+++ include/stdio2.h
@@ -16,7 +16,7 @@ typedef struct {
     uint8_t currbuf[]; // flexible member for current cluster
 } FILE;
 
-FILE *fopen(const char *pathname, const char *mode);
+FILE *fopen(char *pathname, char *mode);
 int fclose(FILE *stream);
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 //size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
